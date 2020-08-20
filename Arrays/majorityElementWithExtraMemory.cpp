f5711bf8ca479e56074c39f4c72a40d6dcf73d8e
reshmidas97 <reshmidas97@gmail.com>
2020-08-20 07:33:34 +0530

    New file

---
+++ Arrays/majorityElementWithExtraMemory.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+https://www.geeksforgeeks.org/majority-element/ 
+with extra memory, Time complexity : O(n)
+*/ 
+int main()
+{
+	int current, current_count, max, index, temp;
+	int size;
+	cin >> size;
+	vector<int> v;
+	vector<int> occurence;
+
+	for(int i = 0; i < size ; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(),v.end());
+
+	cout << endl << "Elements: " << endl;
+	for(int i = 0; i < size ; i++)
+	{
+		cout << v[i] << " ";
+	}
+
+	current = v[0];
+	current_count = 1;
+	cout << endl << current_count << " ";
+	occurence.push_back(current_count);
+	for(int i = 1 ; i < size ; i++)
+	{
+		if(v[i] == current)
+		{
+			current_count++;
+		}
+		else
+		{
+			current = v[i];
+			current_count = 1;
+		}
+		
+		occurence.push_back(current_count);
+		cout << current_count << " ";
+	}
+	cout << endl;
+	max = occurence[0];
+	index = 0;
+	for(int i = 0; i < size ; i++)
+	{
+		cout << occurence[i] << " ";
+		if(occurence[i] > max)
+		{
+			index = i;
+			max = occurence[i];
+		}
+	}
+
+	
+	cout << endl << "Maximum occurence: " << max << " of " << "Element " << occurence[index] << endl;
+	return 0;
+}
