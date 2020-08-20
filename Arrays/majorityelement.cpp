8a13695a517f40cc57ea288797dd59ec994fae70
reshmidas97 <reshmidas97@gmail.com>
2020-08-20 06:37:49 +0530

    Major Element Gfg

---
+++ Arrays/majorityelement.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int current, current_count, max, max_count, temp;
+	int size;
+	cin >> size;
+	vector<int> v;
+
+	for(int i = 0; i < size ; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(),v.end());
+
+	current =  v[0];
+	current_count = 1;
+	max = current;
+	max_count = current_count;
+
+	cout << endl << "Elements: " << endl;
+	for(int i = 0; i < size ; i++)
+	{
+		cout << v[i] << " ";
+	}
+
+	cout << endl << current_count << " ";
+	for(int i = 1 ; i < size ; i++)
+	{
+		if(v[i] == current)
+		{
+			current_count++;
+		}
+		else
+		{
+			current = v[i];
+			current_count = 1;
+		}
+		if(current_count > max_count)
+		{
+			max = current;
+			max_count = current_count;
+		}			
+		
+		
+		cout << current_count << " ";
+	}
+
+	
+	cout << endl << "Maximum occurence: " << max << " " << max_count << endl;
+	return 0;
+}
