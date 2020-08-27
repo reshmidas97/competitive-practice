da698dd8ed14eee111884af21bb2442788ca6e1a
reshmidas97 <reshmidas97@gmail.com>
2020-08-27 08:05:08 +0530

    Postfix

---
+++ Stack/infixToPrefix.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+Question: geeksforgeeks.org/stack-set-2-infix-to-postfix/
+Video : https://www.youtube.com/watch?v=vq-nUF0G4fI
+
+Infix to Prefix Conversion
+*/
+	
+bool isOperand(char ch)
+{
+	if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+		return true;
+	return false;
+}
+
+int checkPrecedence(char ch)
+{
+	if(ch == '^')
+		return 3;
+
+	else if((ch == '*') || (ch == '/'))
+		return 2;
+
+	else if((ch == '+') || (ch == '-'))
+		return 1;
+
+	else
+	{
+		cout <<"Invalid Operator Entered" << endl;
+		exit(0);
+	}
+}
+
+string infixToPrefix(string s)
+{
+	string new_s;
+	stack<char> operators;
+
+	for(int i = 0 ; i < s.length() ; i++)
+	{
+		if(isOperand(s[i]))
+		{
+			new_s += s[i];
+			continue;
+		}
+		
+		if(operators.empty())
+			operators.push(s[i]);
+		else
+		{
+			if(checkPrecedence(s[i]) > checkPrecedence(operators.top()))
+				operators.push(s[i]);
+			else
+			{	while((!operators.empty()) && (checkPrecedence(s[i]) <= checkPrecedence( operators.top() ) ) ) 
+				{
+					new_s += operators.top();
+					operators.pop();
+				}
+				operators.push(s[i]);
+			}
+		}
+	}
+	while(!operators.empty())
+	{
+		new_s += operators.top();
+		operators.pop();
+	}
+	
+	return new_s;
+}
+
+int main()
+{
+	string str;
+	cin >> str;
+	cout << "Infix Expression: " << str << endl;
+	cout << "Postfix Expression: " << infixToPrefix(str) << endl;
+	return 0;
+
+}
